64666163b4486be2d066971f106e7c2d00a741f8
Vitor <vitor.prosperi@outlook.com>
2025-06-23 20:46:38 -0300

    Teste de navegação entre telas

--- dhapp/app/second.js
+++ dhapp/app/second.js
@@ -1,19 +1,13 @@
 import { View, Text, Button } from 'react-native';
 import {useRouter} from 'expo-router';
 
-
 export default function App (){
     const router = useRouter();
 
-    const novapagina = () => {
-        router.push('/second');
-    };
-
-
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Hello World</Text>
-            <Button title='Próximo' onPress={novapagina}/>
+            <Text>Nova Tela</Text>
+            <Button title="Voltar" onPress={router.back} />
         </View>
     )
 } 
