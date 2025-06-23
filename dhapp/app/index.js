fb3dc3a602b817231e1904ac9554bac887d14927
Vitor <vitor.prosperi@outlook.com>
2025-06-23 18:58:08 -0300

    Exclusão de arquivos desnecessários

---
+++ dhapp/app/index.js
@@ -0,0 +1,13 @@
+import { View, Text, Button } from 'react-native';
+
+function novapagina(){
+    
+}
+
+export default function App (){
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Hello World</Text>
+        </View>
+    )
+} 
