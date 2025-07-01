e834ea3dec5333d6b6a26294f75bc553281d1a09
rene <105558751+renefmelo@users.noreply.github.com>
2025-07-01 11:42:58 -0300

    fix: props do ButtonP

--- dhapp/components/ButtonP.tsx
+++ dhapp/components/ButtonP.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { GestureResponderEvent, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-// O jeito que eu fiz esse botao foi com o app tutorial do expo + google entao talvez esteja errado.
-// E a linha gigante definindo o tipo do props foi um quick fix do vscode.
-// Ainda não sei react sinto mto pode mudar essa lógica se vc souber um modo mais bonito. (mas ta funcionando por enquanto)
+// O jeito que eu fiz esse botao foi com o app tutorial do expo + stackoverflow entao talvez esteja errado.
+// Até o momento funciona mas se vc souber fazer mais bonito pode mexer a vontade
 
 type Props = {
     label: string;
+    onPress(): void;
 };
 
 
-export default function ButtonP(props: { onPress: ((event: GestureResponderEvent) => void) | null | undefined; label: string | number | bigint | boolean | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<string | number | bigint | boolean | React.ReactPortal | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | null | undefined> | null | undefined; }) {
+export default function ButtonP({label, onPress}: Props) {
 
     
     return (
         <View>
-            <Pressable style={styles.button} onPress={props.onPress}>
-                <Text style={styles.text}>{props.label}</Text>
+            <Pressable style={styles.button} onPress={onPress}>
+                <Text style={styles.text}>{label}</Text>
             </Pressable>
         </View>
     )
