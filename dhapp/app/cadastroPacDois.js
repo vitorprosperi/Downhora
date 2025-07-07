6703d32183fc80b62737244480220dc0dd249fb8
Vitor <vitor.prosperi@outlook.com>
2025-07-07 13:10:17 -0300

    Começando API dos correios

--- dhapp/app/cadastroPacDois.js
+++ dhapp/app/cadastroPacDois.js
@@ -1,16 +1,19 @@
-import { View, Text, TextInput } from "react-native"
+import { View, Text, TextInput, ScrollView } from "react-native"
 import { useState } from "react";
+import { unidades } from "../unidades/unidades";
+import { cadastropacTres } from "../routes/rotas";
+import DropDownPicker from 'react-native-dropdown-picker';
+import ButtonP from '@/components/ButtonP';
 
 export default function CadastroPacDois() {
+//Variáveis para o funcionamento do dropdown
 const [aberto, Setaberto] = useState(false);
 const [valor, setValor] = useState(null);
-  const [items, setItems] = useState([
-    { label: 'Masculino', value: 'masculino' },
-    { label: 'Feminino', value: 'feminino' },
-  ]);
+const [items, setItems] = useState(unidades);
 
     return(
-        <View>
+    <ScrollView nestedScrollEnabled={true}>
+        <View style={{ padding: 16 }}>
             {/* View da header*/}
             <View>
                 <Text>Cadastro de Pessoas com SD. Down</Text>
@@ -39,7 +42,20 @@ const [valor, setValor] = useState(null);
                 <TextInput style={{ borderWidth: 1, marginBottom: 8 }} />
 
                 <Text>Unidade de Saúde*</Text>
+                <DropDownPicker
+                 open={aberto}
+                 value={valor}
+                 items={items}
+                 setOpen={Setaberto}
+                 setValue={setValor}
+                 setItems={setItems}
+                 placeholder="Selecione a unidade de saúde"
+                 listMode="SCROLLVIEW"
+                 style={{ marginBottom: aberto ? 150 : 8 }} // espaço extra quando aberto
+                />
             </View>
+            <ButtonP label="Continuar" onPress={cadastropacTres}/>
         </View>
+    </ScrollView>
     )
 }
--- dhapp/routes/rotas.js
+++ dhapp/routes/rotas.js
@@ -15,3 +15,7 @@ export function cadastropac () {
 export function cadastropacDois () {
     router.push('/cadastroPacDois');
 }
+
+export function cadastropacTres () {
+    router.push('/cadastroPacTres');
+}
