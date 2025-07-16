9242fff83d0524ec7fae0f783c5f12bbee3c8b2f
rene <105558751+renefmelo@users.noreply.github.com>
2025-07-16 14:43:14 -0300

    att: width formulario pra 80%

--- dhapp/app/cadastroPac.js
+++ dhapp/app/cadastroPac.js
@@ -183,7 +183,7 @@ const styles = StyleSheet.create({
     containerForm: {
         justifyContent: 'flex-start',
         gap: 10,
-        width: 350,
+        width: '80%',
     },
     input: {
         backgroundColor: '#081221',
--- dhapp/components/ButtonP.tsx
+++ dhapp/components/ButtonP.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#FBEC3B',
-        width: 350,
+        width: '100%',
         height: 50,
         borderRadius: 2,
     },
