2e68181d2b01def05925b5b730bdcce31870061d
Vitor Prosperi <vitor.prosperi@outlook.com>
2025-11-03 14:13:14 -0300

    Adicionei imagem

--- dhapp/app/vacina.js
+++ dhapp/app/vacina.js
@@ -1,13 +1,16 @@
-import { Text, View} from "react-native";
+import { Text, View, Image} from "react-native";
 
 
 export default function Vacina() {
   
-
+  const Trabalho = require('@/assets/images/trabalhador.png');
   
     return (
-      <View>
-        <Text>Em Construção</Text>
+    <View>
+      <Image source={Trabalho} style={{ width: 200, height: 200, alignSelf: 'center', marginTop: 50 }} />
+      <View style={{ alignItems: 'center', marginTop: 20 }}>
+        <Text style={{color:'blue'}} >Em Construção</Text>
       </View>
+    </View>
   );
 }
