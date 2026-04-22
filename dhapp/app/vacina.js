12bb6b044a8efe86fa810eb4376cac2a28e7b58e
rene <105558751+renefmelo@users.noreply.github.com>
2026-04-22 11:55:54 -0300

    mudancas pedidas pelo vitor

--- dhapp/app/vacina.js
+++ dhapp/app/vacina.js
@@ -1,4 +1,11 @@
-import { Image, Platform, StatusBar, Text, View } from "react-native";
+import {
+  Image,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const trabalhador = require("@/assets/images/trabalhador.png");
 
@@ -7,12 +14,20 @@ export default function HomeScreen() {
     <View
       style={{
         flex: 1,
+        alignItems: "center",
         padding: 10,
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 10,
       }}
     >
       <Text>Página em manutenção</Text>
-      <Image source={trabalhador}></Image>
+      <Image source={trabalhador} style={styles.image}></Image>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
